b614f41ce2d05513a366bd7ea67c255eb482b53e
Jason McInerney <jmcinerney@opengov.com>
2018-09-06 14:04:00 -0700

    First spike Run a node server

---
+++ spikes/node_http/http_server.js
@@ -0,0 +1,18 @@
+// spiking on an http server, bare bones
+
+var http = require("http");
+
+var server = http.createServer();
+
+server.on("request", function(request, response) {
+    console.log("Received request");
+
+    var body = "<html><head><title>Node HTTP Spike</title></head>" +
+               "<body><p>This is a spike of node http server.</p></body></html>";
+
+    response.end(body);
+});
+
+server.listen(8080);
+
+console.log("Server started");
