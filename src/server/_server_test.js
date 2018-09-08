4b3a89446effc9b6e8c93ffffa3b393d89defb55
Jason McInerney <jmcinerney@opengov.com>
2018-09-08 13:28:17 -0700

    Add test to test a real response

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -10,11 +10,28 @@ exports.tearDown = function(done) {
     });
 };
 
-exports.testHttpServerRespondsToGetRequest = function(test) {
+exports.test_ServerRespondsToGetRequest = function(test) {
     server.start();
     http.get("http://localhost:8080", function(response) {
-        server.stop();
         response.on("data", function() {} );
         test.done();
+    });
+};
+
+exports.test_ServerReturnsHelloWorld = function(test) {
+    server.start(); //TODO: Remove duplicate code
+    var request = http.get("http://localhost:8080");
+    request.on("response", function (response) {
+        var receivedData = false;
+        response.setEncoding("utf8");
+        test.equals(200, response.statusCode, "status code");
+        response.on("data", function(chunk){
+            receivedData = true;
+            test.equals("Hello World", chunk, "this is the expected response" );
+        })
+        response.on("end", function() {
+            test.ok(receivedData, "should have received response");
+            test.done();
+        } );
     });
 };
